e3c4ac0535f01c31cb6e08b15496974adf52ad61
MD. JAHIRUL ISLAM <37324502+SUJON75@users.noreply.github.com>
2020-05-02 18:04:49 +0600

    null vs undefined cleared for interview

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy number
+// "0" " ", []
+
+
+
+const name = " ";
+
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false");
+}
