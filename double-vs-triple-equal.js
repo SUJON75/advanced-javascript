82f480270811732206b19c67d56e63ac4614270a
MD. JAHIRUL ISLAM <37324502+SUJON75@users.noreply.github.com>
2020-05-04 17:15:40 +0600

    object property add

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+const first = 0;
+const second = false;
+
+if(first === second){
+    console.log("condetion is true")
+}
+else{
+    console.log("condition is false")
+}
+
---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const numbers = [3, 4, 5, 6, 7, 8]
+const output = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+function square (element){
+    return element * element;
+}
+
+
+// numbers.map(function(element, index){
+//     console.log(element, index);
+    
+// })
+//console.log(output);
+
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+
+// console.log(result);
+// javascript array map
+const result = numbers.map(x => x * x);
+console.log(result);
+
