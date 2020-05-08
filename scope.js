94e77afce8c0ab2bb0bf72e3269e457a577dff1c
MD. JAHIRUL ISLAM <37324502+SUJON75@users.noreply.github.com>
2020-05-08 10:00:05 +0600

    99% Complete

---
+++ scope.js
@@ -0,0 +1,24 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(bonus);
+    if(result > 9){
+        //const mood = "happy"
+        var mood = "happy"
+
+        mood = "happy1"
+        mood = "happy2"
+        mood = "happy3"
+        mood = "happy4"
+        //console.log(mood);
+
+
+    }
+    console.log(day);
+    let day = "friday";
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
